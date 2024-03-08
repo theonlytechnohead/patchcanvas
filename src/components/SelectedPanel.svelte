222dc5d7a9a13c7c0270838ac55a366d9d8d2f74
Craig Anderson <cranderson0110@gmail.com>
2024-03-08 03:10:31 +0000

    The selected info panel should not be for adding things to nodes

--- src/components/SelectedPanel.svelte
+++ src/components/SelectedPanel.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
-	import { useNodes, useUpdateNodeInternals } from "@xyflow/svelte";
-	import type { ConnectionType } from "./connectionTypes";
+	import { useNodes } from "@xyflow/svelte";
 	const nodes = useNodes();
-
-	const updateNodeInternals = useUpdateNodeInternals();
-
-	function addConnection(node: any, connection: ConnectionType) {
-		node.data.connections.push(connection)
-		node.data = node.data
-		updateNodeInternals(node.id);
-	}
 </script>
 
 {#each $nodes.filter((node) => node.selected) as node}
 	<div>
-		{node.data.label}: {node.data.connection}
-		{node.data.connections}
-		<button on:click={() => addConnection(node, "dante")}>+</button>
+		{node.data.label}: {node.data.connections}
 	</div>
 {/each}
