d0cee3938e002714872212e418f182101a0acb34
Craig Anderson <cranderson0110@gmail.com>
2024-03-08 07:34:22 +0000

    Fix oval handles on Chrome

--- src/components/handles/ConnectionHandle.svelte
+++ src/components/handles/ConnectionHandle.svelte
@@ -7,10 +7,11 @@
 	export let style: string;
 </script>
 
+<!-- it is necessary to manually set the width/height to a specific pixel value because Chrome can't render properly -->
 <Handle
 	{id}
 	type={io === "input" ? "target" : "source"}
 	position={io === "input" ? Position.Top : Position.Bottom}
-	style={style + `background: ${connections[id]};`}
+	style={style + `background: ${connections[id]}; width: 8px; height: 8px;`}
 	isConnectable
 />
