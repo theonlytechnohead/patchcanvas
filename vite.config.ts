300c02b76d87394a7f2efa60216a151146263599
Craig Anderson <cranderson0110@gmail.com>
2025-03-18 08:37:16 +0000

    Configure second entry-point

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      // https://stackoverflow.com/questions/76079913/trying-to-include-multiple-html-pages-in-vite-build
+      input: {
+        main: "index.html",
+        wpbc: "wpbc.html"
+      }
+    }
+  }
 })
