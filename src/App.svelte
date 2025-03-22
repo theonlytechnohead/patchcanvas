7c70133f613f08658ca8ee3fb2d0fd04fdea8e63
Craig Anderson <cranderson0110@gmail.com>
2025-03-22 05:20:16 +0000

    Fix loading first save when mode is mismatched

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import { SvelteFlowProvider } from "@xyflow/svelte";
   import Canvas from "./components/Canvas.svelte";
-  import { current, mode, saves } from "./components/stores";
+  import { current, mode, save, saves } from "./components/stores";
   import { get } from "svelte/store";
 
   // if state says we've got a save from a different mode, load the first save insteead
   if ($current.mode !== $mode) {
-    $current = structuredClone(get(saves)[0]);
+    const safeSaves = Object.values(get(saves)).filter(
+      (save) => save.mode === $mode,
+    );
+    $current = structuredClone(safeSaves.length ? safeSaves[0] : save);
     $current.updated = true;
   }
 
